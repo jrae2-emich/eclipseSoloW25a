7140e156a2999d8386eb1c3de65690e6c6eea453
emu <emu@164.76.205.137>
2025-02-03 12:37:17 -0500

    Programmer 2 update

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Howdy github Project");
 		System.out.println("A second line");
+		System.out.println("Programmer 2 did this.");
 	}
 }
